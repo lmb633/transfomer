5cb69956eeea43f5a7daaa19baed99822feb1409
limingbo <limingbo@focusmedia.cn>
2019-11-11 17:41:56 +0800

    visualize

--- visualize.py
+++ visualize.py
@@ -1,7 +1,7 @@
 import pickle
 import os
 import torch
-from data_gen import AiChallenger2017Dataset,pad_collate
+from data_gen import AiChallenger2017Dataset, pad_collate
 
 
 def sequence_to_text(seq, idx2char):
@@ -24,24 +24,38 @@ if os.path.exists(checkpoint_path):
     checkpoint = torch.load(checkpoint_path)
     model = checkpoint['model']
     model.eval()
+    encoder = model.encoder
+    decoder = model.decoder
+    make_mask = model.make_masks
 
 valid_dataset = AiChallenger2017Dataset('valid')
 valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=1, collate_fn=pad_collate,
-                                               shuffle=True, num_workers=1)
-
+                                           shuffle=True, num_workers=1)
 
 with torch.no_grad():
     for i, batch in enumerate(valid_loader):
-        src, tgt, length = batch
+        src, trg, length = batch
         src = src.to(device)
-        tgt = tgt.to(device)
-        output = model(src, tgt[:, :-1])
-
-        src_text, tgt_text =1,2
-        src_text = sequence_to_text(src_text, src_idx2char)
-        src_text = ' '.join(src_text)
-        print('src_text: ' + src_text)
-
-        tgt_text = sequence_to_text(tgt_text, tgt_idx2char)
-        tgt_text = ' '.join(tgt_text)
-        print('tgt_text: ' + tgt_text)
+        # trg = trg.to(device)
+        print(src)
+        src_mask, trg_mask = make_mask(src, result_sentence)
+        print(src_mask, trg_mask)
+        enc_src = encoder(src, src_mask)
+        print(enc_src.shape)
+        result_sentence = [1]
+        while True:
+            src_mask, trg_mask = make_mask(src, result_sentence)
+            temp_output = decoder(result_sentence, enc_src, trg_mask, src_mask)
+            print(temp_output)
+            if temp_output[-1] == 2:
+                break
+            result_sentence.append(temp_output)
+        print(result_sentence)
+        # src_text, tgt_text = 1, 2
+        # src_text = sequence_to_text(src_text, src_idx2char)
+        # src_text = ' '.join(src_text)
+        # print('src_text: ' + src_text)
+        #
+        # tgt_text = sequence_to_text(tgt_text, tgt_idx2char)
+        # tgt_text = ' '.join(tgt_text)
+        # print('tgt_text: ' + tgt_text)
